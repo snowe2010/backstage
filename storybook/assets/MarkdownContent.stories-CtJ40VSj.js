33d93c9734d09346e0581c43584e244e8a8c00ce
Rugvip <poldsberg@gmail.com>
2024-10-23 10:29:16 +0000

    Deploying to gh-pages from @ backstage/backstage@61562bef00b1e4de2c23e8b9af8e5a5ff812be55 🚀

---
+++ storybook/assets/MarkdownContent.stories-CtJ40VSj.js
@@ -0,0 +1,84 @@
+import{R as u}from"./index-CTjT7uj6.js";import{M as n}from"./MarkdownContent-CrT8aL4k.js";import"./makeStyles-DFHNDtjR.js";import"./defaultTheme-CzJIUhLY.js";import"./index-BRV0Se7Z.js";import"./CodeSnippet-Ck1H52E_.js";import"./iframe-B9cAxeoU.js";import"../sb-preview/runtime.js";import"./createSvgIcon-BxRwmj-P.js";import"./capitalize-CDPu30qc.js";import"./withStyles-Cy0w16fh.js";import"./hoist-non-react-statics.cjs-yZcCaz5O.js";import"./useControlled-BQlqYFiW.js";import"./index-BbmHap-z.js";import"./createSvgIcon-ErQpyllY.js";import"./isMuiElement-Cb6QZSLO.js";import"./unstable_useId-B3Hiq1YI.js";import"./objectWithoutProperties-BHGRdSdY.js";import"./classCallCheck-BNzALLS0.js";import"./inherits-Ax9O35oZ.js";import"./toConsumableArray-C7wLl_Dv.js";import"./useTheme-C1RDaILZ.js";import"./Box-4hiNbBw1.js";import"./typography-DzPlb7_1.js";import"./CopyTextButton-DHea31MM.js";import"./useCopyToClipboard-B-Qz-gIr.js";import"./interopRequireDefault-BbjLfwJi.js";import"./useMountedState-DkESzBh4.js";import"./translation-67MxozS3.js";import"./TranslationApi-eOxINumg.js";import"./ApiRef-BSBwTmJJ.js";import"./Tooltip-CVVhu75l.js";import"./Popper-CCoBcT50.js";import"./Portal-CTbkfrAU.js";import"./Grow-CYgnIZj0.js";import"./utils-Vc_NgNiQ.js";import"./TransitionGroupContext-BtzQ-Cv7.js";import"./IconButton-8IZUBfhx.js";import"./ButtonBase-DtWcvltL.js";const W={title:"Data Display/MarkdownContent",component:n},c=`# GFM
+
+## Autolink literals
+
+www.example.com, https://example.com, and contact@example.com.
+
+## Strikethrough
+
+~one~ or ~~two~~ tildes.
+
+## Table
+
+| foo | bar |
+| --- | --- |
+| baz | bim |
+| buz | bum |
+| biz | bim |
+
+## Tasklist
+
+* [ ] to do
+* [x] done`,p=`# Choreas Iovis
+
+## Incedere retenta
+
+Lorem markdownum velamina [nupta amici aequoreis](http://est-quae.org/sic)
+desertum factum premunt: falcato parvos nihil. Facietque vulnus tum dumque
+reserato Maeandros insignia solidis, tot longi causa et nimium arcuerat altera
+unus, in quis.
+
+1. Est qui dixere nullus
+2. Fuit obicit
+3. Vim patrem portae materiem ulla quod crater
+4. Rigido est magis raptor quid crepitante aequa
+5. Imago quis ignis tamen
+
+## Vix posse vestem
+
+Nec deos robora visa pater toris remittit *crimina* utque, ora ego lacerae quae
+laboris laturus silvas audax terrae. Qua fuisse patrio inlaesas [sine
+seque](http://ambitvictore.org/), nondum et tamen annis, nec. Poscimur magnum,
+Hesperium dedisti, ait ipse et fides terras scalas.
+
+- Quas superis satyri adloquitur natura hausimus
+- Dux suspicere siccare
+- Cape huc quid videor
+- Foret vivit concolor
+- Occupat morte oblectamina minuunt quaeque placidis nate
+- Non posset
+Licet movitque dederat potest in sorores in sola pendere luce pro quod, sit.
+Inpia ut in opibus flores uno quam quo multifidasque fera anhelitus retorsit.
+Sustinui premebat puppe somnos. Dicit genu sic qualia excussit facunde parvae in
+robur, Ianthe Interea. Superis victorque ponat puta cum: est enim.
+
+## Tacetve est in nullis Cerberus silvani luminibus
+
+Divulsere *summissoque esse manes*; artus ausus conatoque utque: illo
+Phaestiadas quod pascat et referentem, nec. In seris, iubebat iam nomina:
+tergoque occidit ingenii.
+
+    mouseDdl(tablet_definition * phishing_icann_mamp);
+    vector += 20 + key_ram.source_isa(hard_tunneling_zone(w_wireless_page));
+    if (rate_client_direct) {
+        textXDpi += sql_cloud_class.sdk.speakers_wired_warm(pcZettabyteGis(
+                market_bezel, 1), 1);
+        tag_scraping = format_ppi;
+    }
+
+Per quem, nec formosior qui cum Peliden me interea **ornatos**! Te facit
+instimulat sequentia in flumina exilium te vulnere, sola. Coetum nec amnes.
+Protinus nam Caras cava, *a* vocantem dicta inevitabile, nata nulla.
+
+## Piscem Iunoni maius
+
+Prece fallere arduus, *ad Athamantis laticem* simillima in ante Temesesque opus!
+Ausim quoslibet crede Tyria: Medusa [muneris Aeneaden
+tutaque](http://cragon-aequoribus.io/) genitor fistula cogeris abstrahere nati,
+relevare videri *non*.
+
+> Promissas ulterius senectae Desinet his ait pedum! Libet *sublime* vibrantia
+> si *dicta quod* pectora cupidine hastam dominoque.
+
+Pedis hic, est bis quod, adhaeret et reditum. Fixa sic vel pugnare **forte est**
+parte in quaerite generisque repugnat; de quod, creatos.`,e=()=>u.createElement(n,{content:p,dialect:"common-mark"}),t=()=>u.createElement(n,{content:c,dialect:"gfm"});e.__docgenInfo={description:"",methods:[],displayName:"MarkdownContentCommonMark"};t.__docgenInfo={description:"",methods:[],displayName:"MarkdownContentGithubFlavoredCommonMark"};var i,a,o;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:'() => <MarkdownContent content={markdown} dialect="common-mark" />',...(o=(a=e.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};var r,s,m;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:'() => <MarkdownContent content={markdownGithubFlavored} dialect="gfm" />',...(m=(s=t.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};const Y=["MarkdownContentCommonMark","MarkdownContentGithubFlavoredCommonMark"];export{e as MarkdownContentCommonMark,t as MarkdownContentGithubFlavoredCommonMark,Y as __namedExportsOrder,W as default};
