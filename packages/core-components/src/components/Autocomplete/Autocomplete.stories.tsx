105f44b4aecc2289dd1587edbf34c36fd4fb642e
Jonathan Roebuck <jroebuck@spotify.com>
2024-10-25 15:41:59 +0100

    Add new core component Autocomplete and use for entity pickers
    Signed-off-by: Jonathan Roebuck <jroebuck@spotify.com>

--- packages/core-components/src/components/Autocomplete/Autocomplete.stories.tsx
+++ packages/core-components/src/components/Autocomplete/Autocomplete.stories.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Backstage Authors
+ * Copyright 2024 The Backstage Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,28 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import TextField, { TextFieldProps } from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
+
 import React from 'react';
-import classnames from 'classnames';
+import { AutocompleteComponent as Autocomplete } from './Autocomplete';
 
-const useStyles = makeStyles(
-  {
-    input: {},
-  },
-  {
-    name: 'CatalogReactEntityAutocompletePickerInput',
-  },
-);
+export default {
+  title: 'Inputs/Autocomplete',
+  component: Autocomplete,
+};
 
-export function EntityAutocompletePickerInput(params: TextFieldProps) {
-  const classes = useStyles();
+export const Default = (args: any) => {
+  return <Autocomplete {...args} />;
+};
 
-  return (
-    <TextField
-      variant="outlined"
-      {...params}
-      className={classnames(classes.input, params.className)}
-    />
-  );
-}
+Default.args = {
+  multiple: true,
+  label: 'Default',
+  name: 'default',
+  options: ['test 1', 'test 2', 'test 3'],
+};
