8c182e8eb208a27d0f33c7c7f07f30ebbafd8f39
Rugvip <poldsberg@gmail.com>
2024-11-02 15:15:55 +0000

    Deploying to gh-pages from @ backstage/backstage@1a04de3f348f0072012716086c8c91e140b25c5a 🚀

---
+++ storybook/assets/Toolkit.stories-Csy5niMl.js
@@ -0,0 +1,30 @@
+import{R as o}from"./index-CTjT7uj6.js";import{c as e}from"./plugin-j4lMXdhf.js";import{S as u}from"./Grid-BsFNIMVV.js";import{C as p}from"./ComponentAccordion-CiPej0pI.js";import{w as g}from"./appWrappers-cqkxjSCE.js";import{T as i}from"./TemplateBackstageLogoIcon-BAFYd9GI.js";import{I as k}from"./InfoCard-dJgQNe9C.js";import"./iframe-obclibUv.js";import"../sb-preview/runtime.js";import"./Plugin-o-ewznXQ.js";import"./componentData-CM4hSmEF.js";import"./ApiRef-BSBwTmJJ.js";import"./useAnalytics-DTrkS1Gy.js";import"./ConfigApi-DBUUc3nU.js";import"./index-CvMVCH8h.js";import"./useRouteRef-UNW_jnDS.js";import"./interopRequireDefault-BbjLfwJi.js";import"./createSvgIcon-BxRwmj-P.js";import"./capitalize-CDPu30qc.js";import"./defaultTheme-CzJIUhLY.js";import"./withStyles-Cy0w16fh.js";import"./hoist-non-react-statics.cjs-yZcCaz5O.js";import"./useControlled-BQlqYFiW.js";import"./index-BbmHap-z.js";import"./createSvgIcon-ErQpyllY.js";import"./isMuiElement-Cb6QZSLO.js";import"./unstable_useId-B3Hiq1YI.js";import"./IconButton-8IZUBfhx.js";import"./ButtonBase-DtWcvltL.js";import"./TransitionGroupContext-BtzQ-Cv7.js";import"./DialogTitle-CBLfs1WT.js";import"./Modal-DOVkv6yD.js";import"./classCallCheck-BNzALLS0.js";import"./Portal-CTbkfrAU.js";import"./Backdrop-DXeLEGLB.js";import"./useTheme-C1RDaILZ.js";import"./utils-Vc_NgNiQ.js";import"./Paper-BK9DXOUE.js";import"./Typography-DtBgvzFq.js";import"./Button-RuG8KIhQ.js";import"./WebStorage-D5Fgivzj.js";import"./index-CFaqwFgm.js";import"./MockTranslationApi-Bm0rhTQl.js";import"./inherits-Ax9O35oZ.js";import"./toArray-BIgjzDMT.js";import"./TranslationApi-eOxINumg.js";import"./ExpandMore-PrXkC_Ga.js";import"./makeStyles-DFHNDtjR.js";import"./AccordionDetails-B1XIbjxN.js";import"./react-is.production.min-D0tnNtx9.js";import"./Collapse-Chzj-FMz.js";import"./useAsync-CXA3qup_.js";import"./useMountedState-DkESzBh4.js";import"./mapValues-r4uG88u0.js";import"./toString-C6iK1gA3.js";import"./ApiProvider-CuNQiN7Z.js";import"./index-BRV0Se7Z.js";import"./CssBaseline-DDRFgeWI.js";import"./ThemeProvider-CVGcooNy.js";import"./jsx-runtime-Cw0GR0a5.js";import"./palettes-B20oCNII.js";import"./index-Cqve-NHl.js";import"./CardActions-CVT7cQhk.js";import"./CardContent-DkZms0rz.js";import"./ErrorBoundary-DZgI1e3N.js";import"./translation-67MxozS3.js";import"./ErrorPanel-cArKPX4u.js";import"./WarningPanel-CxigIcZX.js";import"./MarkdownContent-BSWteIPo.js";import"./CodeSnippet-UJNVNTiG.js";import"./objectWithoutProperties-BHGRdSdY.js";import"./toConsumableArray-C7wLl_Dv.js";import"./Box-4hiNbBw1.js";import"./typography-DzPlb7_1.js";import"./CopyTextButton-DHea31MM.js";import"./useCopyToClipboard-B-Qz-gIr.js";import"./Tooltip-CVVhu75l.js";import"./Popper-CCoBcT50.js";import"./Grow-CYgnIZj0.js";import"./List-DYtqLDXv.js";import"./ListContext-DydK1sOh.js";import"./ListItem-BX8d_s0j.js";import"./ListItemText-DDba65AT.js";import"./LinkButton-Dyk9uPhJ.js";import"./Link-GvJop_Qi.js";import"./lodash-CoGan1YB.js";import"./CardHeader-zP9t5-2f.js";import"./Divider-YusUzn_w.js";import"./BottomLink-CrVzwhZ5.js";import"./ArrowForward-WOguJcOR.js";const or={title:"Plugins/Home/Components/Toolkit",decorators:[m=>g(o.createElement(m,null))]},r=()=>o.createElement(u,{item:!0,xs:12,md:6},o.createElement(e,{tools:Array(8).fill({url:"#",label:"link",icon:o.createElement(i,null)})})),t=()=>{const m=f=>o.createElement(p,{expanded:!0,...f});return o.createElement(k,{title:"Toolkit",noPadding:!0},o.createElement(u,{item:!0},o.createElement(e,{title:"Tools 1",tools:Array(8).fill({url:"#",label:"link",icon:o.createElement(i,null)}),Renderer:m}),o.createElement(e,{title:"Tools 2",tools:Array(8).fill({url:"#",label:"link",icon:o.createElement(i,null)}),Renderer:p}),o.createElement(e,{title:"Tools 3",tools:Array(8).fill({url:"#",label:"link",icon:o.createElement(i,null)}),Renderer:p})))};r.__docgenInfo={description:"",methods:[],displayName:"Default"};t.__docgenInfo={description:"",methods:[],displayName:"InAccordian"};var l,n,a;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`() => {
+  return <Grid item xs={12} md={6}>
+      <HomePageToolkit tools={Array(8).fill({
+      url: '#',
+      label: 'link',
+      icon: <TemplateBackstageLogoIcon />
+    })} />
+    </Grid>;
+}`,...(a=(n=r.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};var c,s,d;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`() => {
+  const ExpandedComponentAccordion = (props: any) => <ComponentAccordion expanded {...props} />;
+  return <InfoCard title="Toolkit" noPadding>
+      <Grid item>
+        <HomePageToolkit title="Tools 1" tools={Array(8).fill({
+        url: '#',
+        label: 'link',
+        icon: <TemplateBackstageLogoIcon />
+      })} Renderer={ExpandedComponentAccordion} />
+        <HomePageToolkit title="Tools 2" tools={Array(8).fill({
+        url: '#',
+        label: 'link',
+        icon: <TemplateBackstageLogoIcon />
+      })} Renderer={ComponentAccordion} />
+        <HomePageToolkit title="Tools 3" tools={Array(8).fill({
+        url: '#',
+        label: 'link',
+        icon: <TemplateBackstageLogoIcon />
+      })} Renderer={ComponentAccordion} />
+      </Grid>
+    </InfoCard>;
+}`,...(d=(s=t.parameters)==null?void 0:s.docs)==null?void 0:d.source}}};const rr=["Default","InAccordian"];export{r as Default,t as InAccordian,rr as __namedExportsOrder,or as default};
